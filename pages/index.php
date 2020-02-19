cdc7c2591acfb0965c03d0c3fa18b381f7775399
kessedias <kesse.dias97@outlook.com>
2020-02-19 12:10:07 -0300

    :rocket: feat: Finalizando estruturacao do plugin

--- pages/index.php
+++ pages/index.php
@@ -7,6 +7,29 @@
  * @author 2020, Kesse Dias <kesse@eadskill.com.br>
  */
 
-//os arquivos config.php e adminlib.php, estipulado pela convenção de desenvolvimento do MOODLE
+//os arquivos config.php e adminlib.php, são estipulados pela convenção de desenvolvimento do MOODLE e devem ser inseridos 
  require_once(__DIR__ . '/../../../config.php');
  require_once($CFG->libdir . '/adminlib.php');
+$CFG->cachejs = false; //não permite js cache
+
+//necessário estar logado para ver esta página
+require_login();
+
+//Cabeçalho - Aqui estou setando o título da página e e o caminho onde o plugin se encontra 
+$PAGE->set_title(get_string ('news_category', $plugin));
+$PAGE->set_heading(get_string('news_category', $plugin));
+$PAGE->set_pagelayout('admin');
+//imprime a saída do cabeçalho
+echo $OUTPUT->header();
+
+//inicio
+$out .= html_writer::start_tag('div');
+echo 'Testando o plugin';
+$out .= html_writer::end_tag('div');
+
+//printando a saída do html
+echo $out;
+
+//printando a saída do rodapé;
+echo $OUTPUT->footer();
+
